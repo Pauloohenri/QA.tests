69fc45a550651136eaa4732b6a5b58af7fe4d99b
Paulo Henrique F <paulohenrik555@gmail.com>
2026-05-06 08:13:05 -0300

    fix: adicionei os  waits no CheckoutPage

--- qa-automation-project/web-tests/pages/checkout_page.py
+++ qa-automation-project/web-tests/pages/checkout_page.py
@@ -1,18 +1,21 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class CheckoutPage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def preencher_dados(self):
-        self.driver.find_element(By.ID, "first-name").send_keys("Paulo")
-        self.driver.find_element(By.ID, "last-name").send_keys("Teste")
-        self.driver.find_element(By.ID, "postal-code").send_keys("12345")
-        self.driver.find_element(By.ID, "continue").click()
+        self.wait.until(EC.presence_of_element_located((By.ID, "first-name"))).send_keys("Paulo")
+        self.wait.until(EC.presence_of_element_located((By.ID, "last-name"))).send_keys("Teste")
+        self.wait.until(EC.presence_of_element_located((By.ID, "postal-code"))).send_keys("12345")
+        self.wait.until(EC.element_to_be_clickable((By.ID, "continue"))).click()
 
     def finalizar(self):
-        self.driver.find_element(By.ID, "finish").click()
+        self.wait.until(EC.element_to_be_clickable((By.ID, "finish"))).click()
 
     def mensagem_sucesso(self):
-        return self.driver.find_element(By.CLASS_NAME, "complete-header").text
+        return self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "complete-header"))).text
