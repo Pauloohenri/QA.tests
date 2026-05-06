e3d476f5cf5a1f1f7e999a90e14603fb4ae77335
Paulo Henrique F <paulohenrik555@gmail.com>
2026-05-06 08:44:58 -0300

    fix: estabiliza o wait de remoçao do carrinho

--- qa-automation-project/web-tests/tests/test_cart.py
+++ qa-automation-project/web-tests/tests/test_cart.py
@@ -1,6 +1,8 @@
 from pages.login_page import LoginPage
 from pages.products_page import ProductsPage
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_adicionar_produto_ao_carrinho(driver):
@@ -11,7 +13,7 @@ def test_adicionar_produto_ao_carrinho(driver):
     produtos.adicionar_produto()
     produtos.ir_para_carrinho()
 
-    itens = driver.find_elements(By.CLASS_NAME, "cart_item")
+    itens = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "cart_item")))
     assert len(itens) > 0
 
 
@@ -23,7 +25,11 @@ def test_remover_produto_do_carrinho(driver):
     produtos.adicionar_produto()
 
     driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
-    driver.find_element(By.CLASS_NAME, "cart_button").click()
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "cart_list")))
+
+    driver.find_element(By.CSS_SELECTOR, "[data-test^='remove']").click()
+
+    WebDriverWait(driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, "cart_item")))
 
     itens = driver.find_elements(By.CLASS_NAME, "cart_item")
     assert len(itens) == 0
