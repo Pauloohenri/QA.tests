3e2ae0ac60116c6a5cc23639b623609dee4de8de
Paulo Henrique F <paulohenrik555@gmail.com>
2026-05-06 21:37:13 -0300

    fix: ajuste no cart page again

--- qa-automation-project/web-tests/pages/cart_page.py
+++ qa-automation-project/web-tests/pages/cart_page.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 class CartPage:
 
     def __init__(self, driver):
@@ -10,11 +9,6 @@ class CartPage:
         self.wait = WebDriverWait(driver, 20)
 
     def iniciar_checkout(self):
-
-        self.wait.until(
-            EC.presence_of_element_located((By.CLASS_NAME, "cart_list"))
-        )
-
         checkout_btn = self.wait.until(
             EC.element_to_be_clickable((By.ID, "checkout"))
         )
@@ -24,8 +18,11 @@ class CartPage:
             checkout_btn
         )
 
-        checkout_btn.click()
+        self.driver.execute_script(
+            "arguments[0].click();",
+            checkout_btn
+        )
 
         self.wait.until(
-            EC.presence_of_element_located((By.ID, "first-name"))
+            EC.url_contains("checkout-step-one")
         )
