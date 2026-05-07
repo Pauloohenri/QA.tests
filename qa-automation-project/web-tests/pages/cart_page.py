2e09d8b6450e95532c84ed6b50974cf45eaa261d
Paulo Henrique F <paulohenrik555@gmail.com>
2026-05-06 21:25:40 -0300

    fix: ajuste no cart page e checkout page

--- qa-automation-project/web-tests/pages/cart_page.py
+++ qa-automation-project/web-tests/pages/cart_page.py
@@ -2,11 +2,30 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class CartPage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 20)
 
     def iniciar_checkout(self):
-        checkout_btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, "checkout")))
+
+        self.wait.until(
+            EC.presence_of_element_located((By.CLASS_NAME, "cart_list"))
+        )
+
+        checkout_btn = self.wait.until(
+            EC.element_to_be_clickable((By.ID, "checkout"))
+        )
+
+        self.driver.execute_script(
+            "arguments[0].scrollIntoView(true);",
+            checkout_btn
+        )
+
         checkout_btn.click()
+
+        self.wait.until(
+            EC.url_contains("checkout-step-one")
+        )
--- qa-automation-project/web-tests/pages/checkout_page.py
+++ qa-automation-project/web-tests/pages/checkout_page.py
@@ -2,20 +2,65 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class CheckoutPage:
 
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(driver, 20)
 
     def preencher_dados(self):
-        self.wait.until(EC.presence_of_element_located((By.ID, "first-name"))).send_keys("Paulo")
-        self.wait.until(EC.presence_of_element_located((By.ID, "last-name"))).send_keys("Teste")
-        self.wait.until(EC.presence_of_element_located((By.ID, "postal-code"))).send_keys("12345")
-        self.wait.until(EC.element_to_be_clickable((By.ID, "continue"))).click()
+
+        self.wait.until(
+            EC.url_contains("checkout-step-one")
+        )
+
+        first_name = self.wait.until(
+            EC.visibility_of_element_located((By.ID, "first-name"))
+        )
+        first_name.clear()
+        first_name.send_keys("Paulo")
+
+        last_name = self.wait.until(
+            EC.visibility_of_element_located((By.ID, "last-name"))
+        )
+        last_name.clear()
+        last_name.send_keys("Teste")
+
+        postal = self.wait.until(
+            EC.visibility_of_element_located((By.ID, "postal-code"))
+        )
+        postal.clear()
+        postal.send_keys("12345")
+
+        continue_btn = self.wait.until(
+            EC.element_to_be_clickable((By.ID, "continue"))
+        )
+
+        self.driver.execute_script(
+            "arguments[0].scrollIntoView(true);",
+            continue_btn
+        )
+
+        continue_btn.click()
 
     def finalizar(self):
-        self.wait.until(EC.element_to_be_clickable((By.ID, "finish"))).click()
+
+        finish_btn = self.wait.until(
+            EC.element_to_be_clickable((By.ID, "finish"))
+        )
+
+        self.driver.execute_script(
+            "arguments[0].scrollIntoView(true);",
+            finish_btn
+        )
+
+        finish_btn.click()
 
     def mensagem_sucesso(self):
-        return self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "complete-header"))).text
+
+        return self.wait.until(
+            EC.visibility_of_element_located(
+                (By.CLASS_NAME, "complete-header")
+            )
+        ).text
